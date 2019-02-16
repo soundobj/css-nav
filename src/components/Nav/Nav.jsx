0a71dde2bb58dd3701f8e652eec5143bd60a244f
soundobj <soundobj@yahoo.com>
2019-02-16 05:46:55 +0100

    implement close nav when clicking outside the using css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -14,25 +14,30 @@ function Nav(props) {
 				onClick={toggle}
 				className={classNames('nav-burger', nav ?
 					"nav-open" : "nav-closed")}
-			>
+				>
 					<Icon
 						name="si-glyph-askterisk"
 					/>
-			</span>
-			<nav className={classNames("nav", nav ?
-				"nav-open" : "nav-closed")}>
-				{props.items.map( (item) => {
-					return (
-						<li key={item.id}
-							className="nav-item"
-							onClick={toggle}
-						>
-							{item.component}
-						</li>
-					)
-				})}
-			</nav>
-		</div>
+				</span>
+				<nav className={classNames("nav", nav ?
+					"nav-open" : "nav-closed")}>
+					<div className="overlay"
+						onClick={toggle}
+						style={{ display: (nav) ? "block" : "none" }}
+					>
+					</div>
+					{props.items.map( (item) => {
+						return (
+							<li key={item.id}
+								className="nav-item"
+								onClick={item.id > 0 ? toggle : null}
+							>
+								{item.component}
+							</li>
+						)
+					})}
+				</nav>
+			</div>
 	);
 }
 
